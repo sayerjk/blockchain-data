8673056abad2422d4e5571a898fe38aa0c11592c
sayerjk <sayerjk@gmail.com>
2022-08-01 11:19:39 -0700

    dict keys now = `KEYS` and `near.py` ultra-clean now.

--- near.py
+++ near.py
@@ -1,12 +1,4 @@
-from near_functions import query_near_testnet, create_dictionary
-import datetime
-
-
-keys = ['emitted_for_receipt_id', 'emitted_at_block_timestamp',
-        'emitted_in_shard_id', 'emitted_index_of_event_entry_in_shard',
-        'emitted_index_of_event_entry_in_shard', 'emitted_by_contract_account_id',
-        'token_id', 'event_kind', 'token_old_owner_account_id', 'token_new_owner_account_id',
-        'token_authorized_account_id', 'event_memo']
+from near_functions import query_near_testnet
 
 query = """SELECT emitted_for_receipt_id, emitted_at_block_timestamp, 
         emitted_in_shard_id, emitted_index_of_event_entry_in_shard,
@@ -16,19 +8,5 @@ query = """SELECT emitted_for_receipt_id, emitted_at_block_timestamp,
         FROM assets__non_fungible_token_events
         LIMIT 1000;"""
 
-result = query_near_testnet(query)
-all_results = []
-
-for item in result:
-    record = create_dictionary(keys, list(item))
-    all_results.append(record)
-
-for item in all_results:
-    if 'emitted_at_block_timestamp' in item.keys():
-        # converting epoch time to datetime string
-        x = str(item['emitted_at_block_timestamp'])
-        mod_string = x[:10]
-        datetime_string = datetime.datetime.fromtimestamp(int(mod_string)).strftime('%Y-%m-%d %H:%M:%S')
-        item['emitted_at_block_timestamp'] = datetime_string
-
-print(all_results)
+results = query_near_testnet(query)
+print(results)
--- near_functions.py
+++ near_functions.py
@@ -3,6 +3,12 @@ from decimal import Decimal
 import datetime
 import psycopg2
 
+KEYS = ['emitted_for_receipt_id', 'emitted_at_block_timestamp',
+        'emitted_in_shard_id', 'emitted_index_of_event_entry_in_shard',
+        'emitted_index_of_event_entry_in_shard', 'emitted_by_contract_account_id',
+        'token_id', 'event_kind', 'token_old_owner_account_id', 'token_new_owner_account_id',
+        'token_authorized_account_id', 'event_memo']
+
 
 class DecimalEncoder(json.JSONEncoder):
     """Converts Decimal() objects to string, allowing JSON storage"""
@@ -19,7 +25,7 @@ def create_dictionary(labels, vals):
     return results
 
 
-def query_near_testnet(sql_query: str) -> dict:
+def query_near_testnet(sql_query: str) -> list[dict]:
     """Query NEAR blockchain based on sql_query() string input"""
     # test NEAR Indexer database info
     conn = psycopg2.connect(host="35.184.214.98",
@@ -28,7 +34,22 @@ def query_near_testnet(sql_query: str) -> dict:
                             password="nearprotocol")
     cur = conn.cursor()
     cur.execute(sql_query)
-    return cur.fetchall()
+    result_tuples = cur.fetchall()
+    all_results = []
+
+    for item in result_tuples:
+        record = create_dictionary(KEYS, list(item))
+        all_results.append(record)
+
+    for item in all_results:
+        if 'emitted_at_block_timestamp' in item.keys():
+            # converting epoch time to datetime string
+            x = str(item['emitted_at_block_timestamp'])
+            mod_string = x[:10]
+            datetime_string = datetime.datetime.fromtimestamp(int(mod_string)).strftime('%Y-%m-%d %H:%M:%S')
+            item['emitted_at_block_timestamp'] = datetime_string
+
+    return all_results
 
 
 def export_json(data: list[dict]):
