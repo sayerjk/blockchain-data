d4443f46e7db0beb6d2e5944800e1cac6738b006
sayerjk <sayerjk@gmail.com>
2022-08-03 18:29:52 -0700

    `bulk_query()` added and used to make .ipynb cleaner

--- near_functions.py
+++ near_functions.py
@@ -59,3 +59,47 @@ def export_json(data: list[dict]):
     timestamp = datetime.datetime.timestamp(now)
     with open(f'data/NEAR_{timestamp}.json', 'w', encoding='utf-8') as f:
         json.dump(data, f, ensure_ascii=False, indent=4, cls=DecimalEncoder)
+
+
+def bulk_query(r):
+    """Grabs output from NEAR CLI filters for media links"""
+    compiled_search_queries = []
+    bulk_results = []
+    for i in r:
+        contract = i['emitted_by_contract_account_id']
+        token = i['token_id']
+        contract = r'\"{}"'.format(contract)
+        token = """{{\"token_id\":\"{}\"}}""".format(token)
+        #         print(contract, token)
+        x = subprocess.run(
+            ["near", "view", contract[1:], "nft_token", token],
+            capture_output=True, text=True
+        ).stdout.strip('\n').lstrip("View call: ")
+
+        new = x.split("\n")
+        new = [i.strip() for i in new]
+
+        found_media = ''
+        for item in new:
+            if 'View call' in item:
+                new.remove(item)
+                continue
+            if 'media:' in item:
+                found_media = item
+            if len(item) < 5:
+                new.remove(item)
+
+        #         print(new)
+        #         print(found_media)
+
+        if len(found_media) > 1 and "null" not in found_media:
+            found_media = found_media.lstrip("media: '")
+            found_media = found_media.rstrip("',")
+            #             print()
+            print(found_media)
+            bulk_results.append(found_media)
+
+    return bulk_results
+
+
+
