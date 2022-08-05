fd3b67864f133faf39d03995de0d243afe5ecb24
sayerjk <sayerjk@gmail.com>
2022-08-05 15:49:28 -0700

    returning jsons from RPC now

--- experiment_setup_rpc.py
+++ experiment_setup_rpc.py
@@ -1,4 +1,6 @@
 from near_api.providers import JsonProvider
 
 near_provider = JsonProvider("https://rpc.testnet.near.org")
-print(near_provider)
+print(near_provider.json_rpc("tx", [
+    "BkPbqf4jR9BYpSYAy47ZsYPNA9vjc6CmHJLqRKz4zHac",
+    "priceoracle.nhtera.testnet"]))
