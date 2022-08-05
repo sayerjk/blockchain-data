c93fdce6565febc4db3c5d5c39a7efe51da29d95
sayerjk <sayerjk@gmail.com>
2022-08-05 16:54:00 -0700

    block query tested

--- experiment_setup_rpc.py
+++ experiment_setup_rpc.py
@@ -1,6 +1,14 @@
 from near_api.providers import JsonProvider
 
 near_provider = JsonProvider("https://rpc.testnet.near.org")
-print(near_provider.json_rpc("tx", [
-    "BkPbqf4jR9BYpSYAy47ZsYPNA9vjc6CmHJLqRKz4zHac",
-    "priceoracle.nhtera.testnet"]))
+print(near_provider.json_rpc("query", {"request_type": "view_state",
+                                       "finality": "final",
+                                       "account_id": "node1",
+                                       "prefix_base64": ""}))
+
+print("Block Query: ")
+
+block = near_provider.get_block(
+    "HYJXpq9SmHg4SskZaXdNQkzjnYpD9wmrmNX2ZH87ZFJk"
+)
+print(block)
