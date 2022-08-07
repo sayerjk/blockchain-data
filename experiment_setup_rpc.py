1e77acd98744dea5aecbddd57f6c7b0e267552b5
sayerjk <sayerjk@gmail.com>
2022-08-07 07:54:12 -0700

    exploring Python RPCs

--- experiment_setup_rpc.py
+++ experiment_setup_rpc.py
@@ -1,19 +1,27 @@
 from near_api.providers import JsonProvider
 
-near_provider = JsonProvider("https://rpc.testnet.near.org")
-print(near_provider.json_rpc("query", {"request_type": "view_state",
-                                       "finality": "final",
-                                       "account_id": "node1",
-                                       "prefix_base64": ""}))
+near = JsonProvider("https://rpc.mainnet.near.org")
 
-print("Block Query: ")
-
-block = near_provider.get_block(
-    "HYJXpq9SmHg4SskZaXdNQkzjnYpD9wmrmNX2ZH87ZFJk"
+print(
+    f"{'**' * 40}\n",
+    f"{near.rpc_addr() = }\n",
+    f"{near.json_rpc('status', params=[]) = }\n",
+    f"{near.get_status() = }\n",
+    f"{near.get_validators() = }\n",
+    # f"{near.query('masternode24.poolv1.near') = }",
+    f"{near.get_account(account_id='masternode24.poolv1.near') = }\n",
+    # `epoch_id` returns from `get_block()`
+    f"{near.get_block(block_id='6jJbQ8A99gAotHKnVbv9D7P5oVzwSzj8xuFFxgD5CLL4') = }\n",
+    f"{near.get_changes_in_block(block_id='6jJbQ8A99gAotHKnVbv9D7P5oVzwSzj8xuFFxgD5CLL4') = }\n",
+    # "stake" field in `get_validators_ordered()`
+    f"{near.get_validators_ordered('6jJbQ8A99gAotHKnVbv9D7P5oVzwSzj8xuFFxgD5CLL4') = }\n",
+    # f"{near.get_light_client_proof('3x1rcwf9mwepSEHBkCjexHNPzEXvNBF1HEdGCDS1PuG2')}",
+    f"{near.get_receipt('HBu8sZuVjMiwNjx9WdjztECMAbije2CUEetNvjA3PqfS') = }\n"
 )
-print(block)
 
-s = 'HszoeEzYx8R5WaFcfSFVeK353TNtrWNibvJ2sMaiytC'
 
-vals = near_provider.get_validators()
-print(vals)
+"""Reference this link for functions: https://github.com/near/near-api-py/blob/master/near_api/providers.py
+    I had to copy/paste the github url code into my local installation of this near-rpc-py library, 
+    because the pip install version does not have the most current code in it. """
+
+"""Grab test data from https://explorer.near.org/blocks"""
